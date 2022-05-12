24240ef692b694cb0a096fc82bdb9bc3f3ee1178
Luca Gandini <lgandini@funambol.com>
2022-05-12 15:19:37 +0200

    Updated description

--- components/description/description.js
+++ components/description/description.js
@@ -1,16 +1,18 @@
-import styles from './description.module.css';
+import styles from "./description.module.css";
 
 const Description = () => {
   return (
     <div className={styles.abstract}>
       <p className={styles.abstractMessage}>
-        Hello, I am a frontend developer... Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        Hello, I am a frontend developer with more than 10 years of experience.
+        I am focused on the development of Web application using the most recent
+        tecnologies and following the best practices. I like web because it is
+        open and free and I still believe it will be the future even in a time
+        where smartphone and mobile apps ruled the world (the same world where
+        Web tecnhologies have been used to develop mobile, desktop and server
+        applications). <br />I also have a lot of interests around the dev world
+        like web accessibility, IoT and embedded development, IT security and
+        cryptography, image editing and 3D graphics.
       </p>
     </div>
   );
