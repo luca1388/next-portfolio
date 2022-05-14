24f99ebfeebc611f4d105023c1b4964880a260f3
Luca Gandini <lgandini@funambol.com>
2022-05-14 12:41:30 +0200

    fixed description

--- components/description/description.js
+++ components/description/description.js
@@ -6,12 +6,12 @@ const Description = () => {
       <p className={styles.abstractMessage}>
         Hello, I am a frontend developer with more than 10 years of experience.
         I am focused on the development of Web applications using the most
-        recent tecnologies and following the best practices. I like web because
-        it is open and free and I still believe it will be the future even in a
-        time where smartphone and mobile apps ruled the world. <br />I also have
-        a lot of interests around the dev world like web accessibility, IoT and
-        embedded development, IT security and cryptography, image editing and 3D
-        graphics.
+        recent tecnologies and following the best practices. I like the Web
+        because it is open and free and I still believe it will be the future
+        even in a time when smartphones and mobile apps rule the world. <br />I
+        also have a lot of interests around the dev world like web
+        accessibility, IoT and embedded development, IT security and
+        cryptography, image editing and 3D graphics.
       </p>
     </div>
   );
